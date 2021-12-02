1f5133a3f99acf67087bc93dc43dde9cf523bca6
rgarciamvm <91730364+rgarciamvm@users.noreply.github.com>
2021-12-02 19:06:46 +0100

    Update data-movies.js

--- share/data-movies.js
+++ share/data-movies.js
@@ -1,6 +1,11 @@
 
 class data_movie{
- // Afegiu codi per 
+ // Afegir les propietats a l'objecte (tipus) perquè moviesController.js pugui passar els paràmetres necessaris a moviesModel.js
+ // Recorda que NO has de modificar NI recodificar app.js NI moviesModel.js
+ 
+ 
+ 
+ 
 }
 
 export default new data_movie();
